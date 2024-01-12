70bee03ec7717e4f14b5e396e704abc1c14eea3e
Tanujnarnam <67031523+Tanujnarnam@users.noreply.github.com>
2024-01-12 16:06:22 -0600

    Teams Page Done

--- front_end/src/components/Positions/index.js
+++ front_end/src/components/Positions/index.js
@@ -15,17 +15,17 @@ const Positions = () => {
     <>
       <h1 className='pos-title'>Positions</h1>
       <div className="positions-div">
-      {
-        positions.map((position) => (
-          <div className='square-div'> 
-            <img src={position.cover} alt="position" className='pos-images'/>
-            <div className='overlay'>
-              <h1 className="position-text">{position.full_name}</h1>
-              <Link className="position-btn" to={`/data?pos=${encodeURIComponent(position.position)}`}>GO</Link>
+        {
+          positions.map((position) => (
+            <div className='square-div'> 
+              <img src={position.cover} alt="position" className='pos-images'/>
+              <div className='overlay'>
+                <h1 className="position-text">{position.full_name}</h1>
+                <Link className="position-btn" to={`/data?pos=${encodeURIComponent(position.position)}`}>GO</Link>
+              </div>
             </div>
-          </div>
-        ))
-      }
+          ))
+        }
       </div>
     </>
   )
--- front_end/src/components/Teams/index.js
+++ front_end/src/components/Teams/index.js
@@ -1,10 +1,34 @@
 import './teams.css';
-import Table from '../Table';
+import {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom'
+import teamData from '../jsons/teams.json'
 
 
 const Teams = () => {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    setTeams(teamData.teams)
+  }, []
+  )
+
   return(
-    <></>
+    <>
+      <h1 className='team-title'>Teams</h1>
+      <div className="teams-div">
+        {
+          teams.map(team => (
+            <div className='square-div'> 
+              <img src={`../teams/${team.short}.png`} alt="team" className='team-images'/>
+              <div className='overlay'>
+                <h1 className="team-text">{team.team}</h1>
+                <Link className="team-btn" to={`/data?team=${encodeURIComponent(team.short)}`}>GO</Link>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 };
 
